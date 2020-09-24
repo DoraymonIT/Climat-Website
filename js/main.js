442decf4e9e6ec55bf66409980ca56bbf0948702
DoraymonIT <bendrimou@gmail.com>
2020-09-24 15:49:25 +0100

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+  AOS.init();
+
+  
+  var options = {
+    strings: ['FROID .', 'CHAUD .'],
+    typeSpeed: 120,
+    loop: true,
+    loopCount: Infinity,
+    cursorChar: '.',
+  };
+  
+  var typed = new Typed('#fc', options);
