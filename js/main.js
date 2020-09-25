f5e2c77c89dc6a65128f01c6c6cbfc0bb402891a
DoraymonIT <bendrimou@gmail.com>
2020-09-25 11:52:33 +0100

    edit home page

--- js/main.js
+++ js/main.js
@@ -1,13 +1,28 @@
+AOS.init();
 
-  AOS.init();
-
-  
   var options = {
     strings: ['FROID .', 'CHAUD .'],
     typeSpeed: 120,
     loop: true,
     loopCount: Infinity,
-    cursorChar: '.',
+    cursorChar: '..',
   };
   
   var typed = new Typed('#fc', options);
+  function animateValue(id, start, end, duration) {
+    var range = end - start;
+    var current = start;
+    var increment = end > start? 1 : -1;
+    var stepTime = Math.abs(Math.floor(duration / range));
+    var obj = document.getElementById(id);
+    var timer = setInterval(function() {
+        current += increment;
+        obj.innerHTML = current;
+        if (current == end) {
+            clearInterval(timer);
+        }
+    }, stepTime);
+}
+
+// animateValue("value", 1, 189, 5);
+
