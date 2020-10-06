435015bc42d2df9a8cd94bce223f1610da8a9127
DoraymonIT <bendrimou@gmail.com>
2020-10-06 16:27:27 +0100

    Update chart.js

--- js/chart.js
+++ js/chart.js
@@ -5,7 +5,7 @@ var myChart = new Chart(ctx, {
         labels: ['2015', '2016', '2017', '2018', '2019', '2020'],
         datasets: [{
             
-            label: ' Notre Chiffre d affaires dans les derniers années ( * 1 000 DH)',
+            label: ' Notre Chiffre d affaires dans les derniers années ( * 1 000 DH)  ',
             data: [120, 190, 133, 150, 200, 130],
             backgroundColor: [
                 'cadetblue',
